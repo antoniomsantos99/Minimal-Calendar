d61985e2ecdd95fcddcdc329288f08267e875f36
António Santos <38131791+antoniomsantos99@users.noreply.github.com>
2018-04-20 19:52:39 +0100

    Update calendar.c
    Translated text and fixed issues with months that used 6 lines

--- calendar.c
+++ calendar.c
@@ -12,60 +12,60 @@ int isBissexto(int year) {
 
 int meses(int month, int year) {
   if (month == 2) {if (isBissexto(year) == 1) {return 29;} else {return 28;}}
-
+  
   if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-      return 31;}
-
-      return 30;}
-
-int primeiroDiaMes (int month, int year) {
-  int n,p;
-  n = (1 + 2*(month) + (3*(month+1)/5) + year + (year/4) - (year/100) + (year/400) + 2) % 7;
-
-  if (n==5) {p=4;}
-  if (n==4) {p=3;}
-  if (n==3) {p=2;}
-  if (n==2) {p=1;}
-  if (n==1) {p=0;}
-  if (n==0) {p=6;}
-  if (n==6) {p=5;}
-  if (n>6 || n<0) {printf("Shit went wrong!");}
- return p;
-
-}
-
-void imprimirDiasSemana() {
-  printf("| D | S | T | Q | Q | S | S |\n");
-}
-
-void meteDias (int month, int year) {
-  int dia = 1;
-  int l,c;
-  int d = primeiroDiaMes(month, year);
-  int h = meses(month, year);
-
-
-
-    for (l = 0; l < 5; l++) {
+    return 31;}
+    
+    return 30;}
+    
+    int primeiroDiaMes (int month, int year) {
+      int n,p;
+      n = (1 + 2*(month) + (3*(month+1)/5) + year + (year/4) - (year/100) + (year/400) + 2) % 7;
+      
+      if (n==5) {p=4;}
+      if (n==4) {p=3;}
+      if (n==3) {p=2;}
+      if (n==2) {p=1;}
+      if (n==1) {p=0;}
+      if (n==0) {p=6;}
+      if (n==6) {p=5;}
+      if (n>6 || n<0) {printf("Shit went wrong!");}
+      return p;
+      
+    }
+    
+    void imprimirDiasSemana() {
+      printf("| D | S | T | Q | Q | S | S |\n");
+    }
+    
+    void meteDias (int month, int year) {
+      int dia = 1;
+      int l,c;
+      int d = primeiroDiaMes(month, year);
+      int h = meses(month, year);
+      
+      
+      
+      for (l = 0; l < 6; l++) {
         for (c = 0; c < 7; c++) {
-            if ((l == 0 && c < d) || dia > h) { printf("    ");} else {
+          if ((l == 0 && c < d) || dia > h) { printf("    ");} else {
             if (dia < 10) { printf("  %d ", dia);}
             else {printf(" %d ", dia);}
             dia++;
-}
+          }
         }
-    printf("\n");
-}
-
-}
-
-
-  void main() {
-    int year, month;
-    printf("Introduza o ano.\n");
-    scanf("%d",&year);
-    printf("Introduza o mês\n");
-    scanf("%d",&month);
-    imprimirDiasSemana();
-    meteDias(month,year);
-  }
+        printf("\n");
+      }
+      
+    }
+    
+    
+    void main() {
+      int year, month;
+      printf("Input the year.\n");
+      scanf("%d",&year);
+      printf("Input the month.\n");
+      scanf("%d",&month);
+      imprimirDiasSemana();
+      meteDias(month,year);
+    }
